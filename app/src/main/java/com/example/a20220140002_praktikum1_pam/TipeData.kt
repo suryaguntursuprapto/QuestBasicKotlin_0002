af30f8b442c9fcf507694df891b24e99edc8086a
Surya Guntur Suprapto <suryaguntursuprapto@MacBook-Pro-Guntur.local>
2024-10-10 10:32:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a20220140002_praktikum1_pam/TipeData.kt
@@ -0,0 +1,43 @@
+package com.example.a20220140002_praktikum1_pam
+
+fun main(){
+
+    //byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    //short
+    val shortValue: Short = 20
+    println("Short: $shortValue")
+
+    //Int
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+    //long
+    val longValue: Long = 40
+    println("Long: $longValue")
+
+    //float
+    val floatValue : Float = 50.6f
+    println("Float: $floatValue")
+
+    //double
+    val doubleValue: Double = 60.6
+    println("Double: $doubleValue")
+
+    //char
+    val charValue: Char = 'A'
+    println(charValue)
+
+    //boolean
+    val booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+    //string
+    var stringValue: String = "Hello, World!"
+    println("String: $stringValue")
+
+    stringValue = "Hello, Kotlin"
+    println("String: $stringValue")
+}
