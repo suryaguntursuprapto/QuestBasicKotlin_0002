cf26b46457adf674074c3d528f0c551d73361cbd
suryaguntursuprapto <suryaguntur2000@gmail.com>
2024-10-11 10:24:50 +0700

    mengcheck kondisi pada null

--- app/src/main/java/com/example/a20220140002_praktikum1_pam/NullSafety.kt
+++ app/src/main/java/com/example/a20220140002_praktikum1_pam/NullSafety.kt
@@ -10,4 +10,19 @@ fun nullSafety(){
     //nullable has nullabe string type
     val nullable: String? = "You can keep a null here"
 
+    //this is ok
+    //nullable = null
+
+    // Check for null in conditions
+    if(neverNull == null){
+        println("inferredNonNull is null")
+    }else{
+        println("inferredNonNull is not null")
+    }
+
+    //safe call operator
+    println(neverNull.length)//18
+    println(nullable?.length)
+
+    //
 }
