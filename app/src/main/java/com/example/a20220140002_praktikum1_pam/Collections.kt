578f8e0c53eb45537fab79010deef615d7ee9025
suryaguntursuprapto <suryaguntur2000@gmail.com>
2024-10-11 08:57:59 +0700

    membuat map

--- app/src/main/java/com/example/a20220140002_praktikum1_pam/Collections.kt
+++ app/src/main/java/com/example/a20220140002_praktikum1_pam/Collections.kt
@@ -64,3 +64,38 @@ fun ContohSet(){
     val shapesLocked: Set<String> = shape
     println(shapesLocked)
 }
+
+// Map adalah kumpulan data yang disusun dalam pasangan key-value
+// Map bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
+// Map Read-Only menggunakan mapOf
+// Map Mutable menggunakan mutableMapOf
+
+fun ContohMap(){
+    println()
+    println("=== Map ===")
+
+    //Map Read-Only
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+    // Map mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    // Menambahkan data ke dalam Map Mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
+    // Menghapus data dari Map Mutable
+    shape.remove("Circle")
+    println(shape)
+
+    // Mengubah data di dalam Map Mutable
+    shape["Square"] = 5
+    println(shape)
+
+    // Map Read-Only
+    val shapesLocked: Map<String, Int> = shape
+    println(shapesLocked)
+
+}
