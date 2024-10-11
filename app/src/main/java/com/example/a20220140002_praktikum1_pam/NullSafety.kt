f5e3a6fae6bb7a43846a20293dcb9df53c2c9f17
suryaguntursuprapto <suryaguntur2000@gmail.com>
2024-10-11 10:27:21 +0700

    notNull tidak boleh menerima nilai null

--- app/src/main/java/com/example/a20220140002_praktikum1_pam/NullSafety.kt
+++ app/src/main/java/com/example/a20220140002_praktikum1_pam/NullSafety.kt
@@ -24,5 +24,11 @@ fun nullSafety(){
     println(neverNull.length)//18
     println(nullable?.length)
 
-    //
+    //notNull doesn't accept null values
+    fun strLength(notNull: String): Int {
+       return  notNull.length
+    }
+
+    println(strLength(neverNull))//18
 }
+
