79039461b9c87fd56cc33cc81a182b93c5641aff
suryaguntursuprapto <suryaguntur2000@gmail.com>
2024-10-11 10:02:18 +0700

    membuat data class

---
+++ app/src/main/java/com/example/a20220140002_praktikum1_pam/DataClass.kt
@@ -0,0 +1,14 @@
+package com.example.a20220140002_praktikum1_pam
+
+// Data class adalah class yang digunakan yntuk menyimpan data.
+// Data class menyediakan fungsi untuk meng-override fungsi equals(), hashCode(). dan toString()
+// Data class juga menyediakan fungsi copy() untuk membuat salinan objek dengan beberapa perubahan.
+
+data class DataClass(
+    val id: Int,
+    var email: String
+)
+
+fun main(){
+
+}
