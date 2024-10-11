f28fe011b450e701134ee50a6708d8a6336a26be
suryaguntursuprapto <suryaguntur2000@gmail.com>
2024-10-11 08:12:07 +0700

    memanggil variabel contact ke function utama dan mengisi datanya

--- app/src/main/java/com/example/a20220140002_praktikum1_pam/Class.kt
+++ app/src/main/java/com/example/a20220140002_praktikum1_pam/Class.kt
@@ -3,3 +3,10 @@ package com.example.a20220140002_praktikum1_pam
 class Motor()
 
 class Contact(val id: Int, var email: String)
+
+fun main(){
+    val contact = Contact(1,"mary@gmail.com")
+
+    //prints the value of the property: email
+    println(contact.email)
+}
