6534098d606d5068088e78bd6b8ee3b785d80dec
suryaguntursuprapto <suryaguntur2000@gmail.com>
2024-10-11 09:09:39 +0700

    conditional if-else

---
+++ app/src/main/java/com/example/a20220140002_praktikum1_pam/ConditionalExpressions.kt
@@ -0,0 +1,15 @@
+package com.example.a20220140002_praktikum1_pam
+
+// Conditional Expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional expression menggunakan if-else, when, dan try-catch
+fun ConditionalStatement(){
+    println("=== Conditional Expression ===")
+
+    // If-Else
+    val number = 10
+    if (number > 0){
+        println("Positive number")
+    }else{
+        println("Negative number")
+    }
+}
